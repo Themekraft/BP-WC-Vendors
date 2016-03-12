644be3d164ce4e56083b91577a9070470bda6398
Sven Lehnert <mail@sven-lehnert.de>
2016-03-12 13:58:09 +0100

    reorder settings page

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -29,20 +29,7 @@ function bp_wc_vendors_screen() { ?>
       <form method="post" action="?page=bp_wc_vendors_screen">
         <div id="post-body-content">
 
-            <h3>BuddyPress WC Vendors Dependencies</h3>
-
-            You need a couple of plugins installed for the BP WC Vendors Social Marketplace to work. Please make sure you have the following plugins installed and activated.
-
-            <ul>
-              <li><b>BuddyPress</b></li>
-              <li><b>WooCommerce</b></li>
-              <li><b>WC Vendor Pro</b></li>
-              <li><b>WC4BP - WooCommerce BuddyPress Integration</b></li>
-              <li><b>BuddyForms</b><li>
-            </ul>
-            <br><br>
-            <h3>Vendor Store Settings</h3>
-            <p>Deactivate Vendor Daschboard Tabs</p>
+            <h3>Deactivate Vendor Daschboard Tabs</h3>
             <?php
 
             $tab_products_disabled = 0;
@@ -76,7 +63,6 @@ function bp_wc_vendors_screen() { ?>
             <p><input name='bp_wc_vendors_options[tab_ratings_disabled]' type='checkbox' value='1' <?php checked( $tab_ratings_disabled, 1  ) ; ?> /> <b>Turn off "Ratings" tab. </b></p>
             <p><input name='bp_wc_vendors_options[tab_coupongs_disabled]' type='checkbox' value='1' <?php checked( $tab_coupongs_disabled, 1  ) ; ?> /> <b>Turn off "Coupongs" tab. </b></p>
             <br>
-            <br>
             <h3>Deactivate WordPress Dashboard for Vendors</h3>
             <p>By default, only vendors will be redirected to their BuddyPress 'Member Profile Vendors Dashboard' if they try to access the backend ( /wp-admin ). All other roles will be able ti acces the wp admin.</p>
             <p>In the WC Vendor Pro settings you can set WordPress Dashboard to "Only administrators can access the /wp-admin/ dashboard".<p>
@@ -84,23 +70,43 @@ function bp_wc_vendors_screen() { ?>
             <p><input name='bp_wc_vendors_options[no_admin_access]' type='checkbox' value='1' <?php checked( $no_admin_access, 1  ) ; ?> /> <b>Turn off the redicet and enable admin backend access. </b></p>
             <br>
             <br>
+
+
+            <h3>BuddyPress WC Vendors Dependencies</h3>
+
+            You need a couple of plugins installed for the BP WC Vendors Social Marketplace to work. Please make sure you have the following plugins installed and activated.
+
+            <ul>
+              <li><b>BuddyPress</b></li>
+              <li><b>WooCommerce</b></li>
+              <li><b>WC Vendor Pro</b></li>
+            </ul>
+            <br>
+            <h3>Works great with</h3>
+            <ul>
+              <li><b>WC4BP - WooCommerce BuddyPress Integration</b></li>
+              <li><b>BuddyForms</b><li>
+            </ul>
+
+
+
             <h3>Frontend Product Management</h3>
             <p>For the product management we need BuddyForms and some extensions.</p>
 
-          <ul>
-            <li><b>BuddyForms</b> Form Builder to build the product forms</li>
-            <li><b>BuddyForms Members</b> to add the product forms to BuddyPress Members Profile</li>
-            <li><b>BuddyForms Moderation</b> to moderate new or edited products</li>
-            <li><b>BuddyForms WooCommerce Form Elements</b> adds all WooCommerce form elements to the form builder</li>
-          </ul>
+            <ul>
+              <li><b>BuddyForms</b> Form Builder to build the product forms</li>
+              <li><b>BuddyForms Members</b> to add the product forms to BuddyPress Members Profile</li>
+              <li><b>BuddyForms Moderation</b> to moderate new or edited products</li>
+              <li><b>BuddyForms WooCommerce Form Elements</b> adds all WooCommerce form elements to the form builder</li>
+            </ul>
 
-          <p>After you have created your product forms we need to set the permissions for each form.
-          The Vendor Stores plugin deactivates capabilities management for the vendor and pending vendor roles.
-          So we need to set the capabilities for each form we want to give vendors access to. The permission section in your form builder will not work for vendors.
-          </p>
+            <p>After you have created your product forms we need to set the permissions for each form.
+            The Vendor Stores plugin deactivates capabilities management for the vendor and pending vendor roles.
+            So we need to set the capabilities for each form we want to give vendors access to. The permission section in your form builder will not work for vendors.
+            </p>
 
-          <br>
-          <input type="submit" value="Save" name="bp_wc_vendors_options_submit" class="button">
+            <br>
+            <input type="submit" value="Save" name="bp_wc_vendors_options_submit" class="button">
         </div>
       </form>
   </div>
