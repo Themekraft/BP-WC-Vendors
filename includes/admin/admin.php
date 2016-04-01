9217070e928ea2ee0057cda6f043b6a861571d42
Sven Lehnert <mail@sven-lehnert.de>
2016-04-01 11:16:51 +0200

    add new options to show hide vendor relevant links visit_store_disabled, view_profile_disabled, sold_by_disabled, Disable Contact Vendor Link

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -19,33 +19,9 @@ function bp_wc_vendors_screen() {
 
   if(isset($_POST['bp_wc_vendors_options_submit']))
       update_option('bp_wc_vendors_options',$_POST['bp_wc_vendors_options']);
-  $bp_wc_vendors_options = get_option('bp_wc_vendors_options');
 
-  $tab_products_disabled = 0;
-  if(isset( $bp_wc_vendors_options['tab_products_disabled']))
-    $tab_products_disabled = $bp_wc_vendors_options['tab_products_disabled'];
+  $bp_wc_vendors_options = get_option('bp_wc_vendors_options'); ?>
 
-  $tab_orders_disabled = 0;
-  if(isset( $bp_wc_vendors_options['tab_orders_disabled']))
-      $tab_orders_disabled = $bp_wc_vendors_options['tab_orders_disabled'];
-
-  $tab_settings_disabled = 0;
-  if(isset( $bp_wc_vendors_options['tab_settings_disabled']))
-    $tab_settings_disabled = $bp_wc_vendors_options['tab_settings_disabled'];
-
-  $tab_ratings_disabled = 0;
-  if(isset( $bp_wc_vendors_options['tab_ratings_disabled']))
-      $tab_ratings_disabled = $bp_wc_vendors_options['tab_ratings_disabled'];
-
-  $tab_coupons_disabled = 0;
-  if(isset( $bp_wc_vendors_options['tab_coupons_disabled']))
-      $tab_coupons_disabled = $bp_wc_vendors_options['tab_coupons_disabled'];
-
-  $no_admin_access = 0;
-  if(isset( $bp_wc_vendors_options['no_admin_access']))
-      $no_admin_access = $bp_wc_vendors_options['no_admin_access'];
-
-  ?>
   <div class="wrap">
     <div id="icon-options-general" class="icon32"><br></div>
     <h2>BuddyPress WooCommerce Vendors</h2>
@@ -53,19 +29,64 @@ function bp_wc_vendors_screen() {
         <div id="post-body-content">
 
             <h3>Deactivate Vendor Daschboard Tabs</h3>
+            <?php isset( $bp_wc_vendors_options['tab_products_disabled'] ) ? $tab_products_disabled = $bp_wc_vendors_options['tab_products_disabled'] : $tab_products_disabled = 0; ?>
             <p><input name='bp_wc_vendors_options[tab_products_disabled]' type='checkbox' value='1' <?php checked( $tab_products_disabled, 1  ) ; ?> /> <b>Turn off "Products" tab. </b></p>
+
+            <?php isset( $bp_wc_vendors_options['tab_orders_disabled'] ) ? $tab_orders_disabled = $bp_wc_vendors_options['tab_orders_disabled'] : $tab_orders_disabled = 0; ?>
             <p><input name='bp_wc_vendors_options[tab_orders_disabled]' type='checkbox' value='1' <?php checked( $tab_orders_disabled, 1  ) ; ?> /> <b>Turn off "Orders" tab. </b></p>
+
+            <?php isset( $bp_wc_vendors_options['tab_settings_disabled'] ) ? $tab_settings_disabled = $bp_wc_vendors_options['tab_settings_disabled'] : $tab_settings_disabled = 0; ?>
             <p><input name='bp_wc_vendors_options[tab_settings_disabled]' type='checkbox' value='1' <?php checked( $tab_settings_disabled, 1  ) ; ?> /> <b>Turn off "Settings" tab. </b></p>
+
+            <?php isset( $bp_wc_vendors_options['tab_ratings_disabled'] ) ? $tab_ratings_disabled = $bp_wc_vendors_options['tab_ratings_disabled'] : $tab_ratings_disabled = 0; ?>
             <p><input name='bp_wc_vendors_options[tab_ratings_disabled]' type='checkbox' value='1' <?php checked( $tab_ratings_disabled, 1  ) ; ?> /> <b>Turn off "Ratings" tab. </b></p>
+
+            <?php isset( $bp_wc_vendors_options['tab_coupons_disabled'] ) ? $tab_coupons_disabled = $bp_wc_vendors_options['tab_coupons_disabled'] : $tab_coupons_disabled = 0; ?>
             <p><input name='bp_wc_vendors_options[tab_coupons_disabled]' type='checkbox' value='1' <?php checked( $tab_coupons_disabled, 1  ) ; ?> /> <b>Turn off "Coupons" tab. </b></p>
             <br>
             <h3>Deactivate WordPress Dashboard for Vendors</h3>
             <p>By default, only vendors will be redirected to their BuddyPress 'Member Profile Vendors Dashboard' if they try to access the back-end ( /wp-admin ). All other roles will be able to access the wp admin.</p>
             <p>In the WC Vendor Pro settings you can set WordPress dashboard to "only administrators can access the /wp-admin/ dashboard".<p>
 
+            <?php isset( $bp_wc_vendors_options['no_admin_access'] ) ? $no_admin_access = $bp_wc_vendors_options['no_admin_access'] : $no_admin_access = 0; ?>
             <p><input name='bp_wc_vendors_options[no_admin_access]' type='checkbox' value='1' <?php checked( $no_admin_access, 1  ) ; ?> /> <b>Turn off the redirect and enable admin back-end access. </b></p>
             <br>
 
+            <h3>Deactivate Links</h3>
+            <p>Deactivate the Visit Store link in the BuddyPress Members Profile Headder</p>
+            <?php isset( $bp_wc_vendors_options['visit_store_disabled'] ) ? $visit_store_disabled = $bp_wc_vendors_options['visit_store_disabled'] : $visit_store_disabled = 0; ?>
+            <p><input name='bp_wc_vendors_options[visit_store_disabled]' type='checkbox' value='1' <?php checked( $visit_store_disabled, 1  ) ; ?> /> <b>visit_store_disabled</b></p>
+
+            <p>Deactivate the View Profile Links in the Vendor Shop Product listings and Products Single Views</p>
+            <?php isset( $bp_wc_vendors_options['view_profile_disabled'] ) ? $view_profile_disabled = $bp_wc_vendors_options['view_profile_disabled'] : $view_profile_disabled = 0; ?>
+            <p><input name='bp_wc_vendors_options[view_profile_disabled]' type='checkbox' value='1' <?php checked( $view_profile_disabled, 1  ) ; ?> /> <b>view_profile_disabled</b></p>
+
+            <p>Deactivate the View Sold by links in the Product Listings and Single View</p>
+            <?php isset( $bp_wc_vendors_options['sold_by_disabled'] ) ? $sold_by_disabled = $bp_wc_vendors_options['sold_by_disabled'] : $sold_by_disabled = 0; ?>
+            <p><input name='bp_wc_vendors_options[sold_by_disabled]' type='checkbox' value='1' <?php checked( $sold_by_disabled, 1  ) ; ?> /> <b>sold_by_disabled</b></p>
+
+            <p>Deactivate the Contact Vendor links in the Product Single View</p>
+            <?php isset( $bp_wc_vendors_options['contact_vendor_disabled'] ) ? $contact_vendor_disabled = $bp_wc_vendors_options['contact_vendor_disabled'] : $contact_vendor_disabled = 0; ?>
+            <p><input name='bp_wc_vendors_options[contact_vendor_disabled]' type='checkbox' value='1' <?php checked( $contact_vendor_disabled, 1  ) ; ?> /> <b>Disable Contact Vendor Link</b></p>
+
+            <br>
+
+            <h3>Vendor Store Settings</h3>
+            <p>Redirect the Vendor Store to the BuddyPress Vendor Profile</p>
+            <?php isset( $bp_wc_vendors_options['redirect_vendor_store_to_profil'] ) ? $redirect_vendor_store_to_profil = $bp_wc_vendors_options['redirect_vendor_store_to_profil'] : $redirect_vendor_store_to_profil = 0; ?>
+            <p><input name='bp_wc_vendors_options[redirect_vendor_store_to_profil]' type='checkbox' value='1' <?php checked( $redirect_vendor_store_to_profil, 1  ) ; ?> /> <b>redirect_vendor_store_to_profil</b></p>
+
+            <p>Integrate into Profiel</p>
+            <?php isset( $bp_wc_vendors_options['integrate_vendor_store_as_profil_tab'] ) ? $integrate_vendor_store_as_profil_tab = $bp_wc_vendors_options['integrate_vendor_store_as_profil_tab'] : $integrate_vendor_store_as_profil_tab = 0; ?>
+            <p><input name='bp_wc_vendors_options[integrate_vendor_store_as_profil_tab]' type='checkbox' value='1' <?php checked( $integrate_vendor_store_as_profil_tab, 1  ) ; ?> /> <b>integrate_vendor_store_as_profil_tab</b></p>
+
+            <p>Select the BuddyForm</p>
+            <?php isset( $bp_wc_vendors_options['integrate_vendor_store_form'] ) ? $integrate_vendor_store_form = $bp_wc_vendors_options['integrate_vendor_store_form'] : $integrate_vendor_store_form = 'none'; ?>
+            <p><select name='bp_wc_vendors_options[integrate_vendor_store_form]'>
+
+                <option>Forms</option>
+            </select></p>
+            <br>
 
             <?php if( defined( 'BUDDYFORMS_VERSION' )){ ?>
               <h3>BuddyForms Settings</h3>
