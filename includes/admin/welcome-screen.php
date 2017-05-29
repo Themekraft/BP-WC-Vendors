7f7e6521af44bdb3ba4131bf694089692de6fa85
Konrad Sroka <cunraid@gmail.com>
2017-05-29 20:49:30 +1000

    welcome screen

--- includes/admin/welcome-screen.php
+++ includes/admin/welcome-screen.php
@@ -15,9 +15,9 @@ function bp_wcv_welcome_screen_content() {
 
 			<h1>Welcome to BP WC Vendors Version <?php echo BP_WCV_VERSION ?></h1>
 
-			<p class="about-text">The BuddyPress Marketplace - Enjoy Groundbreaking New Features!</p>
+			<!-- <p class="about-text">The BuddyPress Marketplace - Enjoy Groundbreaking New Features!</p> -->
 
-            <h2 class="nav-tab-wrapper wp-clearfix">
+      <h2 class="nav-tab-wrapper wp-clearfix">
 				<a href="about.php" class="nav-tab nav-tab-active">What’s New</a>
 				<a href="admin.php?page=bp_wc_vendors_screen-addons" target="_new" title="Browse Add-ons" class="nav-tab">Browse Add-ons</a>
 			</h2>
@@ -25,19 +25,21 @@ function bp_wcv_welcome_screen_content() {
 
 			<div class="feature-section two-col" style="margin: 30px 0; overflow: auto;">
 
-				<div class="xcol col-big">
+				<!-- <div class="xcol col-big">
 					<h2>A Revolutionary Customer Experience</h2>
 					<p class="lead">
-						<b>Never feel lost again.</b> Let your USERS find all in one Place and Make the BuddyPress Profile a Home for your Vendors and Customers
+						<b>Never feel lost again.</b> Let your USERS find all in one place, for a truly seamless Marketplace experience.
 					</p>
-				</div>
+				</div> -->
 
-                <div class="xcol col-big">
-                    <h2>The final collection of Plugins for your Premium Marketplace Bundled and Supported in one Plugin</h2>
-                    <p class="lead">
-                        With BuddyPress, WooCommerce + WC4BP, WC Vendor + BP WC Vendors and BuddyForms to create beautiful and intuitive Product Forms you have all tools to your hands you need to build the next outstanding Marketplace.
-                    </p>
-                </div>
+        <div class="xcol col-big">
+					<!-- With BuddyPress, WooCommerce + WC4BP, WC Vendor + BP WC Vendors and BuddyForms to create beautiful and intuitive Product Forms you have all tools to your hands you need to build the next outstanding Marketplace. -->
+					<h1>Your Premium Marketplace.</h1>
+					<h3>Bundled and supported in one plugin.</h3>
+          <p class="lead">This plugin will guide you through the Marketplace set up you need to get it all working smoothly with BuddyPress, WooCommerce and WC Vendors. <br>
+							And to top it off, we will show you how you make better product submission forms with BuddyForms.
+					</p>
+        </div>
 
 			</div>
 
@@ -46,9 +48,9 @@ function bp_wcv_welcome_screen_content() {
             <div class="feature-section two-col" style="margin: 30px 0; overflow: auto;">
 
                 <div class="xcol col-big">
-                    <h2>First Step install all Dependencies</h2>
+                    <h2>First Step - install all dependencies</h2>
                     <p class="lead">
-                        We recommend you to install and activate all Plugins from the list below. Just check all and select install/ activate in the Bulk Actions
+                        We recommend you to install and activate all plugins from the list below. Just check all and select install/activate in the Bulk Actions menu.
                     </p>
                     <?php $jhg = new TGM_Plugin_Activation; $jhg->install_plugins_page(); ?>
                 </div>
@@ -58,14 +60,14 @@ function bp_wcv_welcome_screen_content() {
             <div class="feature-section two-col" style="margin: 30px 0; overflow: auto;">
 
                 <div class="xcol col-big">
-                    <h2>Next Step: Setup All!</h2>
+                    <h2>Next Step: Set it all up</h2>
                     <p class="lead">
                         Go to the <a href="<?php echo admin_url('admin.php?page=bp_wc_vendors_screen') ?>" >Plugin Settings</a> and Set up all to your needs. You can find Documentation and Help for the different plugins from the links below.
                     </p>
 
                 </div>
 
-                <div class="bfw-section bfw-getting-started">
+                <div class="bfw-section bfw-getting-started" style="overflow: auto;">
                     <h3>Documentation</h3>
                     <div class="bfw-col bfw-col-50">
                         <div class="well">
@@ -82,36 +84,45 @@ function bp_wcv_welcome_screen_content() {
                             <h3 class="bfw-title">ThemeKraft</h3>
                             <ul>
                                 <li><a href="https://docs.themekraft.com/collection/208-wc4bp-integration" target="_blank"><b>WooCommerce BuddyPress Integration</b></a></li>
-                                <li><a href="https://docs.themekraft.com/collection/474-bp-wc-vendors" target="_blank"><b>WC BP Vendors</b></a></li>
+                                <li><a href="https://docs.themekraft.com/collection/474-bp-wc-vendors" target="_blank"><b>BP WC Vendors</b></a></li>
                                 <li><a href="https://docs.buddyforms.com" target="_blank"><b>BuddyForms</b></a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
 
+								<div style="padding: 20px 20px; background: #fff; border: 1px solid rgba(0,0,0,0.1); margin: 50px 0 20px;">
+									<p class="lead"><b>Need Help?</b></p>
+									<p class="lead"><a href="#" title="Get the whole setup done, by pros." target="_blank" class="button button-primary">Done-For-You Service</a></p>
+									<p class="lead">Save time and do it properly.</p>
+								</div>
+
             </div>
 
-            <hr style="margin: 30px 0;">
+            <!-- <hr style="margin: 30px 0;"> -->
 
-			<!-- Blogpost & Changelog -->
-			<div class="bfw-section bfw-news" style="margin-top: 30px;">
-				<div class="bfw-col bfw-col-50">
-					<h2 class="bfw-title">Latest Blogpost</h2>
-					<p class="lead">Read all about Marketplaces with BuddyPress. Find Tips and Tricks in our Blog:</p>
-					<a href="https://themekraft.com/news/" target="_new" class="button button-primary">Read Blogpost</a>
-				</div>
-				<div class="bfw-col bfw-col-50">
-					<h2 class="bfw-title">Changelog</h2>
-					<p class="lead">Check out the changelog for version <?php echo BP_WCV_VERSION ?></p>
-					<a href="https://wordpress.org/plugins/bp-wc-vendors/#developers" target="_new" class="button button-primary">View Changelog</a></p>
-				</div>
-			</div>
-
-
-			<hr style="margin: 140px 0;">
+						<!-- Blogpost & Changelog -->
+						<div class="bfw-section bfw-news" style="margin-top: 30px;">
+							<div class="bfw-col bfw-col-50">
+								<h2 class="bfw-title">Latest Blogpost</h2>
+								<p class="lead">Read all about Marketplaces with BuddyPress. Find Tips and Tricks in our Blog:</p>
+								<a href="https://themekraft.com/news/" target="_new" class="button button-primary">Read Blogpost</a>
+							</div>
+							<div class="bfw-col bfw-col-50">
+								<h2 class="bfw-title">Changelog</h2>
+								<p class="lead">Check out the changelog for version <?php echo BP_WCV_VERSION ?></p>
+								<a href="https://wordpress.org/plugins/bp-wc-vendors/#developers" target="_new" class="button button-primary">View Changelog</a></p>
+							</div>
+						</div>
 
 
-            <h2>GO PRO and get the most out of your Marketplace</h2>
+						<hr style="margin: 140px 0;">
+
+
+            <div style="padding: 20px 20px; background: #fff; border: 1px solid rgba(0,0,0,0.1); margin: 50px 0 20px;">
+							<h2 style="margin: 20px 0; padding: 0;">Taking it seriously?</h2>
+							<p style="font-size: 17px;">Get the PRO versions of those plugins:</p>
+						</div>
 
             <div class="feature-section two-col" style="margin: 30px 0; overflow: auto;">
 
@@ -129,7 +140,8 @@ function bp_wcv_welcome_screen_content() {
                              src="<?php echo BP_WCV_PLUGIN_URL . 'assets/admin/images/bp-wcv-banner-1544x500.jpg'?>"
                              alt="Frontend Product Forms">
                     </div>
-                    <a href="https://themekraft.com/products/woocommerce-buddypress-vendors-integration/" target="_blank">Get BP WC Vendors Pro Now</a> - <a href="https://www.wcvendors.com/product/wc-vendors-pro/" target="_blank">Get WC Vendors Pro Now</a>
+										<a href="https://themekraft.com/products/woocommerce-buddypress-vendors-integration/" target="_blank" class="button button-primary">Get BP WC Vendors PRO Here</a>
+                    <a href="https://www.wcvendors.com/product/wc-vendors-pro/" target="_blank"  class="button button-primary">Get WC Vendors PRO Here</a>
                 </div>
 
             </div>
@@ -141,7 +153,7 @@ function bp_wcv_welcome_screen_content() {
                 <div class="xcol col-big">
                     <h2>All the "My Account" Pages in the BuddyPress Profile</h2>
                     <p class="lead">
-                        Integrate the My Account with BuddyPress Member Profiles and let your Customers find all in one place
+                        Integrate the My Account with BuddyPress Member Profiles and let your Customers find all in one place, with the WooCommerce BuddyPress Integration Plugin.
                     </p>
                 </div>
 
@@ -152,7 +164,7 @@ function bp_wcv_welcome_screen_content() {
                              src="<?php echo BP_WCV_PLUGIN_URL . 'assets/admin/images/wc4bp-banner-1544x500.jpg'?>"
                              alt="Frontend Product Forms">
                     </div>
-                    <a href="https://themekraft.com/products/woocommerce-buddypress-integration/" target="_blank">Get WC4BP Pro Now</a>
+                    <a href="https://themekraft.com/products/woocommerce-buddypress-integration/" target="_blank" class="button button-primary">Get WooCommerce BuddyPress Integration PRO Here</a>
                 </div>
 
             </div>
@@ -179,7 +191,7 @@ function bp_wcv_welcome_screen_content() {
                              alt="Frontend Product Forms">
                     </div>
 
-                    <a href="https://themekraft.com/buddyforms/" target="_blank">Get BuddyForms Pro Now</a>
+                    <a href="https://themekraft.com/buddyforms/" target="_blank" class="button button-primary">Get BuddyForms PRO Here</a>
                 </div>
 
             </div>
